62ebc2b5a75763e09c4bcaa9105b79af8a8b1c42
Dhvanesh Shah <dhvaneshshah@Egens-MacBook-Pro.local>
2021-07-13 13:24:01 -0500

    Add Modal

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,37 +1,47 @@
 import React from 'react';
 import useStyles from './styles';
 import { Typography, Card, CardActions, CardContent, Button } from '@material-ui/core';
+import AnimatedModal from '../Modal/Modal';
 
 
-const SimpleCard = ({id, name, email, desc}) => {
+const SimpleCard = ({ id, name, email, desc }) => {
   const classes = useStyles();
 
+  const handleOpen = () => {
+
+  }
+
   return (
-    <Card className={classes.root}>
-      <CardContent>
+    <div>
+      <Card className={classes.root}>
+        <CardContent>
 
-        <Typography >
-          ID: {id}
-        </Typography>
+          <Typography >
+            ID: {id}
+          </Typography>
 
-        <Typography>
-          Name: {name}
-        </Typography>
+          <Typography>
+            Name: {name}
+          </Typography>
 
-        <Typography noWrap>
-          Description: {desc}
-        </Typography>
+          <Typography noWrap>
+            Description: {desc}
+          </Typography>
 
-        <Typography>
-          Email: {email}
-          <br />
-        </Typography>
+          <Typography>
+            Email: {email}
+            <br />
+          </Typography>
 
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={handleOpen} ><AnimatedModal /></Button>
+          <Button size="small">Delete</Button>
+        </CardActions>
+      </Card>
+      
+    </div>
+
   );
 }
 
